f8d8b6d7b2fe737b07d7f1bfe71ce8f6437c592f
sagarnage123 <sagernage1@gmail.com>
2026-06-22 11:31:27 +0530

    First non-blocking socket version

--- server.cpp
+++ server.cpp
@@ -1,17 +1,19 @@
-#include<iostream>
+#include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
 #include<cstring>
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<unistd.h>
-
+#include<fcntl.h>
 using namespace std;
+
+
 
 int main()
 {
-    int serverFd=socket(AF_INET,SOCK_STREAM,0);
-    if(serverFd<0)
+    int socketId=socket(AF_INET,SOCK_STREAM,0);
+    if(socketId<0)
     {
-        cout<<"Erro occured\n";
+        cout<<"Socket Creation failed\n";
         return 0;
     }
     sockaddr_in serverAddress;
@@ -19,27 +21,49 @@ int main()
     serverAddress.sin_port = htons(8080);
     serverAddress.sin_addr.s_addr = INADDR_ANY;
 
-    if(bind(serverFd,(struct sockaddr*)&serverAddress,sizeof(serverAddress))<0)
+    if(bind(socketId,(struct sockaddr*)&serverAddress,sizeof(serverAddress))<0)
     {
         cout<<"Binding failed\n";
         return 0;
-
     }
 
-    listen(serverFd,5);
-    int clientFd=accept(serverFd,nullptr,nullptr);
-    if(clientFd<0)
+    cout<<"Listeining\n";
+    int fl=fcntl(socketId,F_GETFL);
+    fl|=O_NONBLOCK;
+    fcntl(socketId,F_SETFL,fl);
+
+    listen(socketId,5);
+
+    int clientId=accept(socketId,nullptr,nullptr);
+    int saftey=10000
+
+    while(clientId<0 && saftey>0)
     {
-        cout<<"Connection failed\n";
-        return 0;
+        cout<<"Connect the client\n"<<"Returned value : "<<clientId<<endl;
+        clientId=accept(socketId,nullptr,nullptr);
+        saftey--;
     }
-    char buffer[1024]={0};
-    while(1)
+    cout<<"Client Connceted\n";
+    char buffer[1028]={0};
+    bool flag=true;
+    cout<<"Send message if you want to or press exit to stop\n";
+    while(flag)
     {
-        recv(clientFd,buffer,sizeof(buffer),0);
-        cout<<"Message from client: "<<buffer<<endl;
-    }
-    close(serverFd);
+        memset(buffer,0,sizeof(buffer));
+        int bytesReceived = recv(clientId,buffer,sizeof(buffer),0);
 
+        if(bytesReceived<0)
+        {
+            cout<<"Receiving failed\n";
+            close(socketId);
+            return 0;
+        }
+
+        if(strcmp(buffer,"exit\n")==0)
+        flag=false;
+        cout<<buffer;
+        
+    }
+    close(socketId);
     return 0;
 }
