4515965766b9b2cd1445598fdc8f7020ef3f7eb2
Sagar Nage <sagernage1@gmail.com>
2026-06-26 06:02:04 +0000

    Up to Date files

--- socket_test.cpp
+++ socket_test.cpp
@@ -1,21 +1,29 @@
 #include<iostream>
+#include<cstring>
 #include<sys/socket.h>
+#include<fcntl.h>
 #include<unistd.h>
 #include<netinet/in.h>
 #include<cerrno>
-#include<fcntl.h>
-#include<cstring>
-
+#include<sys/epoll.h>
 using namespace std;
 
+void makeNonBlock(int fd)
+{
+     int fl=fcntl(fd,F_GETFL);
+    fl|=O_NONBLOCK;
+    fcntl(fd,F_SETFL,fl);
+
+}
 int main()
 {
     int socketFd=socket(AF_INET,SOCK_STREAM,0);
     if(socketFd<0)
     {
-        cout<<"Socket Creation Failed\n";
+        cout<<"Socket Creation Failed\n"<<strerror(errno)<<endl;
         return 0;
     }
+    cout<<"Socket Created ...\n";
     sockaddr_in socketAddr;
     socketAddr.sin_family=AF_INET;
     socketAddr.sin_port=htons(8080);
@@ -23,42 +31,84 @@ int main()
 
     if(bind(socketFd,(struct sockaddr*)&socketAddr,sizeof(socketAddr))<0)
     {
-        cout<<"Binding Failed\n";
+        cout<<strerror(errno)<<endl;
+         close(socketFd);
         return 0;
     }
-    int fl=fcntl(socketFd,F_GETFL);
-    fl|=O_NONBLOCK;
-    fcntl(socketFd,F_SETFL,fl);
-    cout<<"Binding Success and now listening in non-blocking mode\n";
-    listen(socketFd,5);
-    int clientFd=-1;
-    while(clientFd<0)
-    {
-        clientFd=accept(socketFd,nullptr,nullptr);
-        cout<<strerror(errno)<<"\n";
-        sleep(1);
-    }
-    cout<<"Client Connected with fd : "<<clientFd<<" Errno is :"<<strerror(errno)<<endl;
-    fl=fcntl(clientFd,F_GETFL);
-    fl|=O_NONBLOCK;
-    fcntl(clientFd,F_SETFL,fl);
+    cout<<"Socket Bind successfully\n";
 
-    char buffer[1028]={0};
-    bool flag=true;
-    while(flag)
+    makeNonBlock(socketFd);
+
+    listen(socketFd,5);
+    cout<<"Listening .... \n";
+
+    int epfd=epoll_create1(0);
+    struct epoll_event ev;
+    ev.events=EPOLLIN;
+    ev.data.fd=socketFd;
+
+    if(epoll_ctl(epfd,EPOLL_CTL_ADD,socketFd,&ev)<0)
     {
-        int n=recv(clientFd,buffer,sizeof(buffer),0);
-        if(n<0)
-        {
-            cout<<"Errno is : "<<strerror(errno);
-            sleep(1);
-        }
-        
-        if(strcmp(buffer,"exit\n")==0)
-        flag=false;
-        cout<<buffer;
+        cout<<strerror(errno)<<endl;
+        close(epfd);
+        close(socketFd);
+        return 0;
     }
+    int n=64;
+    struct epoll_event events[n];
+    char buffer[1048]={0};
+    while(true)
+    {
+        int len=epoll_wait(epfd,events,n,-1);
+
+        for(int i=0;i<len;i++)
+        {
+            if(events[i].data.fd==socketFd)
+            {
+                int clientFd=accept(socketFd,nullptr,nullptr);
+                if(clientFd<0)
+                {
+                    cout<<strerror(errno)<<endl;
+                    continue;
+                }
+                makeNonBlock(clientFd);
+
+                struct epoll_event ev;
+                ev.events=EPOLLIN;
+                ev.data.fd=clientFd;
+
+                if(epoll_ctl(epfd,EPOLL_CTL_ADD,clientFd,&ev)<0)
+                {
+                    cout<<strerror(errno)<<endl;
+                    continue;
+
+                }
+                cout<<endl<<"New Entry with Fd : "<<clientFd<<endl;
+            }
+            else{
+                int clientFd=events[i].data.fd;
+                memset(buffer,0,sizeof(buffer));
+
+                int cur=recv(clientFd,buffer,sizeof(buffer),0);
+                if(cur<=0)
+                {
+                    cout<<strerror(errno)<<endl;
+                    close(epfd);
+                    close(socketFd);
+                    return 0;
+                }
+                else cout<<"Message from: "<<clientFd<<"->"<<buffer;
+                
+            }
+        }
+
+    }
+
     
     close(socketFd);
+    close(epfd);
+
+    
+
     return 0;
 }
