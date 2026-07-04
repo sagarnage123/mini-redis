f61ee77e9691e5909dedd2e2cce1581de348edae
Sagar Nage <sagernage1@gmail.com>
2026-07-04 06:41:03 +0000

    Code transfer from server to socket_tes

--- socket_test.cpp
+++ socket_test.cpp
@@ -11,18 +11,6 @@ using namespace std;
 
 int MAX_CONNECTIONS=64;
 
-void makeNonBlock(int fd)
-{
-    if(fd<0)
-    return;
-
-    int flag=fcntl(fd,F_GETFL);
-    fcntl(fd,F_SETFL,flag|O_NONBLOCK);
-}
-void printError()
-{
-    cout<<strerror(errno)<<endl;
-}
 enum class RespType
 {
     ARRAY,
@@ -33,6 +21,27 @@ enum class RespType
     EMPTY,
     INVALID
 
+};
+enum class ParseStatus
+{
+    OK,
+    NEED_MORE_DATA,
+    ERROR
+
+};
+struct ParseArrayHeader
+{
+    ParseStatus status;
+    int arrayLength;
+    int bytesConsumed;
+
+
+};
+struct ParseBulkString
+{
+    ParseStatus status;
+    string bulkString;
+    int bytesConsumed;
 };
 class Client
 {
@@ -47,29 +56,129 @@ class Client
     {
         this->fd=fd;
     }
-
+    
 };
+
 RespType getRespType(string &inputBuffer)
 {
     if(inputBuffer.size()==0)
     return RespType::EMPTY;
-
+    
     if(inputBuffer[0]=='*')
     return RespType::ARRAY;
-
+    
     if(inputBuffer[0]=='$')
     return RespType::BULK_STRING;
-
+    
     if(inputBuffer[0]=='+')
     return RespType::SIMPLE_STRING;
-
+    
     if(inputBuffer[0]==':')
     return RespType::INTEGER;
-
+    
     if(inputBuffer[0]=='-')
     return RespType::ERROR;
-
+    
     return RespType::INVALID;
+}
+
+ParseArrayHeader parseHeader(string &s)
+{
+    ParseArrayHeader res;
+    if(getRespType(s) != RespType::ARRAY)
+    {
+        res.status=ParseStatus::ERROR;
+        return res;
+    }
+    int cnt=0;
+    for(int i=1;i<s.size()-1;i++)
+    {
+        if(s[i]=='\r' && s[i+1]=='\n')
+        {
+            res.status=ParseStatus::OK;
+            res.bytesConsumed=i+2;
+            res.arrayLength=cnt;
+            return res;
+        }
+        if(s[i]>='0' && s[i]<='9')
+        {
+            cnt=cnt*10+(s[i]-'0');
+        }
+        else{
+            
+            res.status=ParseStatus::ERROR;
+            return res;
+        }
+    }
+    res.status=ParseStatus::NEED_MORE_DATA;
+    return res;
+}
+ParseBulkString parseString(string &s)
+{
+    ParseArrayHeader isArray=parseHeader(s);
+    ParseBulkString res;
+    if(isArray.status!=ParseStatus::OK)
+    {
+        res.status=isArray.status;
+        return res;
+    }
+    int i=isArray.bytesConsumed;
+    if(i>=s.size() || i+1>=s.size())
+    {
+        res.status=ParseStatus::NEED_MORE_DATA;
+        return res;
+    }
+    if(s[i]!='$' || !(s[i+1]>='1' && s[i+1]<='9'))
+    {
+        res.status=ParseStatus::ERROR;
+        return res;
+    }
+    i++;
+    int len=0;
+    for(;i<s.size()-1;i++)
+    {
+        if(s[i]=='\r' && s[i+1]=='\n')
+        {
+            break;
+        }
+        if(s[i]>='0' && s[i]<='9')
+        {
+            len=len*10+(s[i]-'0');
+        }
+        else{
+            res.status=ParseStatus::ERROR;
+            return res;
+        }
+    }
+    i+=2;
+    if(i+len+2>s.size())
+    {
+        res.status=ParseStatus::NEED_MORE_DATA;
+        return res;
+    }
+    if(s[i+len]!='\r' || s[i+len+1]!='\n')
+    {
+        res.status=ParseStatus::ERROR;
+    }
+    for(int idx=i;idx<i+len;idx++)
+    {
+        res.bulkString+=s[idx];
+    }
+    res.bytesConsumed=i+len+2;
+    return res;
+
+}
+void makeNonBlock(int fd)
+{
+    if(fd<0)
+    return;
+
+    int flag=fcntl(fd,F_GETFL);
+    fcntl(fd,F_SETFL,flag|O_NONBLOCK);
+}
+void printError()
+{
+    cout<<strerror(errno)<<endl;
 }
 int main()
 {
@@ -166,28 +275,45 @@ int main()
                         
 
                     }
-                    RespType val=getRespType(user.inputBuffer);
-                    switch (val)
+                    ParseArrayHeader res=parseHeader(user.inputBuffer);
+                    if(res.status==ParseStatus::OK)
                     {
-                        case RespType::EMPTY:cout<<"EMPTY\n";
-                            break;
-                        case RespType::ARRAY:cout<<"Array\n";
-                            break;
-                        case RespType::BULK_STRING:cout<<"BULK_STRING\n";
-                            break;
-                        case RespType::SIMPLE_STRING :cout<<"SIMPLE_STRING\n";
-                            break;
-                            case RespType::INTEGER:cout<<"INTEGER\n";
-                                break;
-                        case RespType::ERROR:cout<<"ERROR\n";
-                            break;
-                        case RespType::INVALID:cout<<"INVALID\n";
-                            break;
-                        
-                        default:
-                            break;
+                        cout<<"Array Length : "<<res.arrayLength<<endl;
+                        cout<<"BytesConsumed : "<<res.bytesConsumed<<endl;
+                        auto bsres=parseString(user.inputBuffer);
+                        if(bsres.status==ParseStatus::OK)
+                        {
+                            cout<<"Bulk String : "<<bsres.bulkString<<endl;
+                            cout<<"Bytes : "<<bsres.bytesConsumed<<endl;
+                        }
+                        else if(bsres.status==ParseStatus::NEED_MORE_DATA)
+                        continue;
+                        else{
+                             cout<<"Formate Error\n";
+                            close(fd);
+                            epoll_event ev;
+                            ev.events=EPOLLIN;
+                            ev.data.fd=fd;
+                            epoll_ctl(epfd,EPOLL_CTL_DEL,fd,&ev);
+
+                        }
                     }
-                    user.inputBuffer="";
+                    else if(res.status==ParseStatus::NEED_MORE_DATA)
+                    {
+                        cout<<"Need More Data\n";
+                        continue;
+
+                    }
+                    else 
+                    {
+                        cout<<"Formate Error\n";
+                        close(fd);
+                        epoll_event ev;
+                        ev.events=EPOLLIN;
+                        ev.data.fd=fd;
+                        epoll_ctl(epfd,EPOLL_CTL_DEL,fd,&ev);
+                    }
+                    cout<<user.inputBuffer<<endl;
 
                 }
                 else if(bytes==0)
@@ -214,6 +340,7 @@ int main()
                         hash.erase(fd);
                     }
                 }
+
 
             }
         }
